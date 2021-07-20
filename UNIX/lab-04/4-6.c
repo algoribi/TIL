903922af4da2b04ea8750496890da4183f74fe7e
algoribi <algoribi36@gmail.com>
2021-07-20 18:33:47 +0900

    Delete unnecessary files

--- UNIX/lab-04/4-6.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-	rename("linux", "LINUX"); // rename을 통해 linux라는 이름을 LINUX로 바꾼다.
-
-	return 0;
-}
--- UNIX/lab-04/4-7.c
+++
@@ -1,7 +0,0 @@
-#include <unistd.h>
-
-int main(void) {
-	rmdir("programming"); // rename을 통해 programming이라는 이름을 가진 디렉토리를 제거한다.
-
-	return 0;
-}
