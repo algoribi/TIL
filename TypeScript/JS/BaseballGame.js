ab34dde0cc3b1ad7bafdbe5784d412e8463c13d9
ShinhyeChoi <algoribi36@gmail.com>
2022-02-22 12:44:42 +0900

    Update BaseballGame.js

--- TypeScript/JS/BaseballGame.js
+++ TypeScript/JS/BaseballGame.js
@@ -1,126 +1,168 @@
 const readline = require('readline');
-const { start } = require('repl');
+
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+    input: process.stdin,
+    output: process.stdout
 });
 
-let chkNumber = [false, false, false, false, false, false, false, false, false, false];
-let ansNumber = [], counterTime = 1, chkReGame = 0;
-let ansFlag = 0; // 0 : 정답이 아닌 상태, 1 : 정답을 맞힌 상태, 2 : 사용자가 게임을 포기한 상태
-let zeroArr = JSON.stringify([0, 0, 0]);
+BaseballGameStart();
 
-infoPrint();
-gameStart();
+function BaseballGameStart() {
+    let answerNumberArray = NewGameSetting();
+    let askNewGameUsersAns;
 
-function infoPrint() {
-    console.log("-------------⚾숫자 야구 게임⚾-------------");
-    console.log("* 사용되는 숫자는 0 ~ 9의 서로 다른 숫자입니다.");
-    console.log("* 숫자는 맞지만 위치가 틀렸을 때는 볼");
-    console.log("* 숫자와 위치가 전부 맞으면 스트라이크");
-    console.log("* 숫자와 위치가 전부 틀리면 아웃");
-    console.log("* '0 0 0'을 입력 시에 포기하는 것으로 간주하고 답을 출력해 줍니다.");
-    console.log("⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾");
-}
+    rl.on('line', function(userInput) {
+        const userNumber = userInput.split(' ');
+            if (userInput == 'give up') {
+                console.log(`정답 : ${answerNumberArray}\n`);
 
-function gameStart() {
-    let input = [];
+                AskNewGame()
+                .then(function(returnUserAns) {
+                    askNewGameUsersAns = returnUserAns;
+                    rl.pause();
+                }).catch(() => {
+                    console.log("* 입력 오류로 게임을 종료합니다.");
+                    rl.close();
+                });
 
-    setAnswerNumber();
+            } else if (!CheckInputFormat(userNumber)) {
+                InputGuidePrint();
+            } else if (CompareUserNumberAndAnswerNumber(answerNumberArray, userNumber)) {
 
-    console.log("> 숫자를 입력해 주세요.");
-    rl.on('line', function (line) {
-        input = line.split(' ').map((el) => parseInt(el));
-        
-        if (chkReGame == 0) {
-            chkAnswer(input);
+                AskNewGame()
+                .then(function(returnUserAns) {
+                    askNewGameUsersAns = returnUserAns;
+                    rl.pause();
+                }).catch(() => {
+                    console.log("* 입력 오류로 게임을 종료합니다.");
+                    rl.close();
+                });
 
-            input.splice(0, input.length - start);
-            counterTime++;
-            
-            if (ansFlag != 0) {
-                reGamePrint();
-                chkReGame = 1;
-            } else 
-                console.log("> 숫자를 입력해 주세요.");
-        } else {
-            if (input[0] == 1) {
-                input.splice(0, input.length - start);
-                gameReSet();
             } else {
-                console.log("-------게임을 종료합니다!-------");
-                rl.close();
+                console.log("> 숫자를 입력해 주세요.");
             }
+    })
+    .on('pause', function() {
+        if (askNewGameUsersAns) {
+            answerNumberArray = NewGameSetting();
+            rl.resume();
+        } else {
+            console.log("--------게임을 종료합니다.--------");
+            rl.close();
         }
-    });
-    rl.on('close', function () {
+    })
+    .on('close', function() {
         process.exit();
     });
 }
 
-function setAnswerNumber() {
-    let pickNum, counterNum = 0;
-    while (counterNum < 3) {
-        pickNum = Math.floor(Math.random() * 10);
-        if (chkNumber[pickNum] == false) {
-            chkNumber[pickNum] = true;
-            ansNumber.push(pickNum);
-            counterNum++;
-        }
-    }
+function NewGameSetting() {
+    GameTitlePrint();
+    return MakeAnswerNumber();
 }
 
-function chkAnswer(arr) {
-    let numberI = [false, false, false, false, false, false, false, false, false, false]; // 사용자의 룰 미숙지로 중복 값 입력 시, s혹은 b의 카운트의 중복을 막기 위해 체크
-    let s = 0, b = 0;
-    for (let i = 0; i < 3; i++) {
-        if (numberI[arr[i]] == false && arr[i] == ansNumber[i])
-            s++;
-        else if (numberI[arr[i]] == false && chkNumber[arr[i]] == true)
-            b++;
-        numberI[arr[i]] = true;
-    }
-    
-    // output
-    if (JSON.stringify(arr) === zeroArr) { // 사용자가 게임을 포기한 경우
-        console.log("😵😵😵Give UP!!!😵😵😵");
-        console.log("정답 : " + ansNumber + "\n");
-        ansFlag = 2;
-    } else if (counterTime == 1 && s == 3) { // 1회차 만에 맞혔을 경우
-        console.log("💥⚾💥⚾홈~~~~~~~런💥⚾💥⚾");
-        console.log("🙆‍♀️🙆‍♂️🙆‍♀️🙆‍♂️🙆‍♀️🙆‍♂️🙆‍♀️🙆‍♂️🙆‍♀️🙆‍♂️🙆‍♀️🙆‍♂️\n");
-        ansFlag = 1;
-    } else if (counterTime > 1 && s == 3) { // 정답을 맞혔을 경우
-        console.log(`✔ ${counterTime}회차 도전에 정답을 맞히셨습니다!`);
-        console.log("🙆‍♀️🙆‍♂️🙆‍♀️🙆‍♂️🙆‍♀️🙆‍♂️🙆‍♀️🙆‍♂️🙆‍♀️🙆‍♂️🙆‍♀️🙆‍♂️\n");
-        ansFlag = 1;
-    } else if (s == 0 && b == 0) { // 아웃일 경우
-        console.log(`✔ ${counterTime}회차 도전 결과 : OUT!!\n`)
-    } else {
-        console.log(`✔ ${counterTime}회차 도전 결과 : ${s}S ${b}B\n`);
-    }
-}
-
-function reGamePrint() {
-    console.log("⚾⚾⚾ 게임 종료!!! ⚾⚾⚾");
-    console.log("⚾ 새로운 게임을 진행하시겠습니까?");
-    console.log("- Yes는 1을, No는 아무 값을 입력해 주세요.");
-}
-
-function gameReSet() {
-    ansNumber.splice(0, 3);
-
-    for (let i = 0; i < 10; i++) {
-        if (chkNumber[i] == true)
-            chkNumber[i] = false;
-    }
-
-    setAnswerNumber();
-    
-    counterTime = 1;
-    ansFlag = 0;
-    chkReGame = 0;
+function GameTitlePrint() {
+    console.log("\n----------------숫자 야구 게임----------------");
+    console.log("* 사용되는 숫자는 0 ~ 9의 서로 다른 숫자입니다.");
+    console.log("* 숫자는 맞지만 위치가 틀렸을 때는 볼");
+    console.log("* 숫자와 위치가 전부 맞으면 스트라이크");
+    console.log("* 숫자와 위치가 전부 틀리면 아웃\n");
     
     console.log("-------새 게임을 시작합니다!-------");
     console.log("> 숫자를 입력해 주세요.");
+}
+
+function MakeAnswerNumber() {
+    const number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const answerNumberArray = [];
+
+    for (let i = 10; i > 7; i--) {
+        const pickNum = Math.floor(Math.random() * i);
+        answerNumberArray.push(number[pickNum]);
+        number.splice(pickNum, 1);
+    }
+
+    return answerNumberArray;
+}
+
+function CheckInputFormat(userArr) {
+    let checkInputForm = true;
+
+    if (userArr.length != 3) {
+        checkInputForm = false;
+    } else {
+        for (let i = 0; i < 3; i++) {
+            if (userArr[i].length != 1 || !isNumeric(userArr[i])) {
+                checkInputForm = false;
+            } else if (i != 0 && userArr[i - 1] == userArr[i]) {
+                checkInputForm = false;
+            }
+        }
+    }
+
+    return checkInputForm;
+}
+
+function InputGuidePrint() {
+    console.log("---------------------입력 안내--------------------------")
+    console.log("* 입력은 공백을 구분으로 세 개의 숫자를 입력받습니다.");
+    console.log("* 숫자는 0부터 9 사이의 양의 자연수입니다.");
+    console.log("* 중복 입력은 허용하지 않습니다. ex) 1 1 2 => 1의 중복 입력");
+    console.log("* 'give up'을 입력 시에 게임을 포기하는 것으로 간주하고 답을 출력합니다.\n");
+
+    console.log("> 옳은 값으로 다시 입력해 주세요.")
+}
+
+function CompareUserNumberAndAnswerNumber(answerArr, userArr) {
+    let strike = 0;
+    let ball = 0;
+
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            if (parseInt(userArr[i]) == answerArr[j] && i == j) {
+                strike++;
+            } else if (parseInt(userArr[i]) == answerArr[j] && i != j) {
+                ball++;
+            }
+        }
+    }
+
+    PrintCompareResult(strike, ball);
+
+    if (strike == 3) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function isNumeric(val) {
+    return /^-?\d+$/.test(val);
+}
+
+function PrintCompareResult(strike, ball) {
+    if (strike == 3) {
+        console.log(`✔ 정답을 맞히셨습니다!`);
+    } else if (strike == 0 && ball == 0) {
+        console.log(`✔ 도전 결과 : OUT!!\n`)
+    } else {
+        console.log(`✔ 도전 결과 : ${strike}S ${ball}B\n`);
+    }
+}
+
+function AskNewGame() {
+    console.log("----------게임 종료----------");
+    console.log("* 새로운 게임을 진행하시겠습니까?");
+    
+    return new Promise(function (resolve, reject) {
+        rl.question('* Yes or No 를 입력해 주세요 : ', (userInput) => {
+            if (userInput == "Yes" || userInput == "yes" || userInput == "YES") {
+                resolve(true);
+            } else if (userInput == "No" || userInput == "no" || userInput == "NO") {
+                resolve(false);
+            } else {
+                reject();
+            }
+        });
+    });
 }
